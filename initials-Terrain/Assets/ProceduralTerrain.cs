400587b81398d19d000c4698d25e4360a18517f4
scottbing <bingscott@gmail.com>
2022-04-28 20:25:10 -0600

    Initial_Terrains
    Initial_Terrains

--- initials-Terrain/Assets/ProceduralTerrain.cs
+++ initials-Terrain/Assets/ProceduralTerrain.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class ProceduralTerrain : MonoBehaviour
 {
     [SerializeField]
-    int xWidth = 30;
+    int xSquares = 30;
     [SerializeField]
-    int zDepth = 30;
+    int zSquares = 30;
 
     Vector3[] vertices;
     Mesh mesh;
@@ -22,9 +22,7 @@ public class ProceduralTerrain : MonoBehaviour
         //his code is wrong when he adds 1, but if you do want to change it 
         //so the depth is number of squares created not numebr of vertices,
         //uncomment the next 2 lines
-        xWidth += 1;
-        zDepth += 1;
-
+        
         mesh= new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         CreateGeometry();
@@ -38,20 +36,41 @@ public class ProceduralTerrain : MonoBehaviour
     }
 
     void CreateGeometry() {
+        //if you want the numberinput to be the number of squares
+        // not vertices, leave the +1 in the next tow lines.
+        int xWidth = xSquares + 1;
+        int zDepth = zSquares + 1;
         int verticeCounter = 0;
 
         vertices = new Vector3[(xWidth) * (zDepth)];
-        for (int x = 0; x < xWidth; x++)
+        for (int z = 0; z < zDepth; z++)
         {
-             for (int z = 0; z < zDepth; z++)
+             for (int x = 0; x < xWidth; x++)
             {
-                vertices[verticeCounter] = new Vector3(x, 1.0f, z);     verticeCounter++;
+                vertices[verticeCounter] = new Vector3(x, 1.0f, z);     
                 verticeCounter++;
             }
         }
         //triangles can only be defined after
         // vertices since they point to vertices
-        triangles = new int[] { 0, 1, 2 };
+        triangles = new int[(xWidth-1) * (zDepth-1) * 6];
+        int vertexcounter = 0; 
+        int trianglecount = 0; 
+        for (int z = 0; z < zDepth-1; z++)
+        {
+            for (int x = 0; x < xWidth-1; x++)
+            {
+                triangles[2 + trianglecount] = vertexcounter + 0; 
+                triangles[1 + trianglecount] = vertexcounter + xSquares + 1; 
+                triangles[0 + trianglecount] = vertexcounter + 1; 
+                triangles[5 + trianglecount] = vertexcounter + 1; 
+                triangles[4 + trianglecount] = vertexcounter + xSquares + 1; 
+                triangles[3 + trianglecount] = vertexcounter + xSquares + 2; 
+                vertexcounter++;
+                trianglecount += 6;
+            }
+            vertexcounter++;
+        }
     }
 
     public void UpdateMesh()
